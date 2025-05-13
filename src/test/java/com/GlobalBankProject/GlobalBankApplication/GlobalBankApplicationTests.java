206826306ef14f42b12a1e7dfe3eeb2f48682154
SujithaChandran1702 <sujichandran1702@gmail.com>
2025-05-13 11:09:45 +0530

    Initial  commit

---
+++ src/test/java/com/GlobalBankProject/GlobalBankApplication/GlobalBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GlobalBankProject.GlobalBankApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
